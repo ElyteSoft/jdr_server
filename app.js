5adaa4f77c39ce82edd617c77f269f5c3d9d23dc
oscarquintanilla <oscar.david.quintanilla@gmail.com>
2023-10-21 20:50:24 -0600

    Configured cross policy error

--- app.js
+++ app.js
@@ -6,19 +6,23 @@ require('dotenv').config();
 
 const port = process.env.PORT || 3000;
 
-app.use('/', routes);
+const corsOptions = {
+  origin: '*',
+  credentials: true,            //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+}
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.options('*', cors());
 
 app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
-  
-
-app.listen(port, () => {
-    console.log(`Running on port ${port}`);
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
+app.use('/', routes);
+
+app.listen(port, () => {
+  console.log(`Running on port ${port}`);
+});
